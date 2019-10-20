f50ee9be00518ca7fbdfce2839f17d8068446311
Elizaveta Borisova <lizkaboriska@gmail.com>
2019-10-20 11:21:19 +0300

    Add bubble sort

--- js/script1.js
+++ js/script1.js
@@ -1,1 +1,16 @@
 console.log("Hello world!");
+function BubbleSort(arr) {
+  let swap;
+  do {
+    swap = false;
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] > arr[i + 1]) {
+        let buff = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = buff;
+        swap = true;
+      }
+    }
+  } while (swap);
+  return arr;
+}
